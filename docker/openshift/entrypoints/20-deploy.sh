20435ee19fb1cacba6e570d2b0e1f8f72ba2c40b
Juho Lehmonen <juho.lehmonen@siili.com>
2023-02-03 11:32:13 +0200

    UHF-7871: Revert deploy script to disable automatic reinstall.

--- docker/openshift/entrypoints/20-deploy.sh
+++ docker/openshift/entrypoints/20-deploy.sh
@@ -13,20 +13,20 @@ if [ ! -n "$OPENSHIFT_BUILD_NAME" ]; then
   exit 1
 fi
 
+# Generate twig caches.
+if [ ! -d "/tmp/twig" ]; then
+  drush twig:compile || true
+fi
+
 # This script is run every time a container is spawned and certain environments might
 # start more than one Drupal container. This is used to make sure we run deploy
 # tasks only once per deploy.
 if [ "$(drush state:get deploy_id)" != "$OPENSHIFT_BUILD_NAME" ]; then
   drush state:set deploy_id $OPENSHIFT_BUILD_NAME
   # Put site in maintenance mode during deploy
-  #drush state:set system.maintenance_mode 1 --input-format=integer
+  drush state:set system.maintenance_mode 1 --input-format=integer
   # Run maintenance tasks (config import, database updates etc)
-  #drush deploy
-
-  # Re-install site with every deployment.
-  drush sql:drop -y
-  drush si minimal --existing-config
-
+  drush deploy
   # Disable maintenance mode
-  #drush state:set system.maintenance_mode 0 --input-format=integer
+  drush state:set system.maintenance_mode 0 --input-format=integer
 fi
